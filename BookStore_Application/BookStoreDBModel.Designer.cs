27048cd32d375ac65e5c075d38628905b6fb3ff6
HaCurly <hacurly13@gmail.com>
2024-07-04 06:50:09 +0700

    committed

--- BookStore_Application/BookStoreDBModel.Designer.cs
+++ BookStore_Application/BookStoreDBModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Documents\TOPS\StepSQLFinal\BookStore_Application\BookStore_Application\BookStoreDBModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\User\Desktop\Seyha Project ADO\BookStore_Application\BookStore_Application\BookStoreDBModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
